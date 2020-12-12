208328cd69dc5f6e7f27d28ea2fc4dc88c34a22a
Muhammad Usman <sheikhusman1864@gmail.com>
2020-12-13 00:13:13 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ var checkSessionAuth = require("../middleware/checkSessionAuth");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-	let products = await Product.find();
+	let products = await product.find();
 	res.render("products/list", { products });
 });
 router.get("/add", checkSessionAuth, async function (req, res, next) {
